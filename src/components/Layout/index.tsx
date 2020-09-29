ba77ca79b92d9cb3f3f5fe16e04170e0721a1292
Jean Mendonça <pierrybm@gmail.com>
2020-09-29 19:50:22 +0000

    Initial setup

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Container } from './styles'
+import LeftColumn from '../LeftColumn'
+import MiddleColumn from '../MiddleColumn'
+import RightColumn from '../RightColumn'
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <main>
+        <LeftColumn />
+      </main>
+    </Container>
+  )
+}
+
+export default Layout
